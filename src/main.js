dac2e406e5282246dbeea8eab0fffda8ad6e1bb9
Saponciou <sap@polidius.dev>
2022-11-21 20:43:48 +0100

    Added Next Steps section🚀
    Beautified the old design [Commit (0.0.2)]

--- src/main.js
+++ src/main.js
@@ -5,11 +5,82 @@ import Listr from 'listr';
 import path from 'path';
 
 
-async function debug(string){
+async function debug(string) {
     console.log(chalk.grey(`   ${string}`));
     return;
 }
 
+async function showHelpAfterFinishing(options) {
+    // get current os
+    const os = require('os');
+    const currentOS = os.platform();
+    // if windows
+    if (options.docker) {
+        console.log(`
+
+${chalk.green.bold(' Next Steps: ')}
+
+    ${chalk.yellow.bold('cd')} ${chalk.white.italic(options.folderName)}
+    ${chalk.yellow.bold('docker-compose up')}${chalk.blue.italic(' --build')}
+
+    And open your browser at: ${chalk.blue.italic('http://localhost:5085')}
+        `);
+    }
+    else {
+        if (currentOS === 'win32') {
+        console.log(`
+
+${chalk.green.bold(' Next Steps: ')}
+
+    # Install dependencies
+    ${chalk.yellow.bold('cd')} ${chalk.white.italic(options.folderName)}
+    ${chalk.yellow.bold('virtualenv env')}
+    ${chalk.yellow.bold('.\\env\\Scripts\\activate')}
+    ${chalk.yellow.bold('pip install -r requirements.txt')}
+
+    # CMD
+    ${chalk.blue.bold('set')} FLASK_APP=app.py
+    ${chalk.blue.bold('set')} FLASK_ENV=development
+
+    # Powershell
+    ${chalk.blue.bold('$env:FLASK_APP')} = ".\\run.py"
+    ${chalk.blue.bold('$env:FLASK_ENV')} = "development"
+
+    # Run the app
+    ${chalk.yellow.bold('flask run')}
+    // or with https
+    ${chalk.yellow.bold('flask run --cert=adhoc')}
+
+    And open your browser at: ${chalk.blue.italic('http://localhost:5000')}
+
+
+        `);
+        }
+        if (currentOS === 'linux' || currentOS === 'darwin' || currentOS === 'freebsd') {
+    console.log(`
+
+${chalk.green.bold(' Next Steps: ')}
+
+    # Install dependencies
+    ${chalk.yellow.bold('cd')} ${chalk.white.italic(options.folderName)}
+    ${chalk.blue.bold('source')} env/bin/activate
+    ${chalk.yellow.bold('pip install -r requirements.txt')}
+
+    # Set environment variables
+    ${chalk.blue.bold('export')} FLASK_APP=run.py
+    ${chalk.blue.bold('export')} FLASK_ENV=development
+
+    # Run the app
+    ${chalk.yellow.bold('flask run')}
+    // or with https
+    ${chalk.yellow.bold('flask run --cert=adhoc')}
+
+    And open your browser at: ${chalk.blue.italic('http://localhost:5000')}
+
+        `);
+        }
+    }
+}
 async function downloadTemplate(options) {
     const template = `https://github.com/app-generator/${options.template}.git`;
     debug(`Starting the download process`);
@@ -40,5 +111,6 @@ export async function createProject(options) {
     );
     await tasks.run();
     console.log('%s Project ready!', chalk.green.bold('  ✔  DONE'));
+    await showHelpAfterFinishing(options);
     return true;
 }
